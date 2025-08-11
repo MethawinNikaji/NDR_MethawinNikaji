a87cb8403f76340636b43f432e0e22efa445b93b
MethawinNikaji <118377996+MethawinNikaji@users.noreply.github.com>
2025-08-11 17:32:25 +0700

    ADD

---
+++ DiscountPolicy.java
@@ -0,0 +1,8 @@
+public interface DiscountPolicy {
+    /*
+     * @param amount the original amount
+     * return the amount after the discount is applied
+     */
+    double applyDiscount(double amount);    
+}
+
